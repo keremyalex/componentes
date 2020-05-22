5bd8271724f06a3c3e55f20cb252880c23a61f4a
Alex <keremy.alex@gmail.com>
2020-05-22 11:10:24 -0400

    6 13 Navegar por pantallas

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
 import 'package:componentes/src/utils/icono_string_util.dart';
+import 'package:componentes/src/pages/alert_page.dart';
 
 class HomePage extends StatelessWidget {
   
@@ -23,7 +24,7 @@ class HomePage extends StatelessWidget {
        //initialData: [],
        builder: ( context, AsyncSnapshot<List<dynamic>> snapshot){
           return ListView(
-            children: _listaItems(snapshot.data),
+            children: _listaItems(snapshot.data, context),
           );
        }
     );
@@ -31,7 +32,7 @@ class HomePage extends StatelessWidget {
     
   }
 
-  List<Widget>_listaItems(List<dynamic> data) {
+  List<Widget>_listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
 
     data.forEach((opt) {
@@ -39,7 +40,13 @@ class HomePage extends StatelessWidget {
         title: Text(opt['texto']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: (){},
+        onTap: (){
+
+          final route = MaterialPageRoute(
+            builder: (context) => AlertPage()
+          );
+          Navigator.push(context, route);
+        },
       );
 
       opciones.add(widgetTemp);
