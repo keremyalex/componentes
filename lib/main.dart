b08bb6a3de410316aa3689ca65347dcd4cf6f483
Alex <keremy.alex@gmail.com>
2020-05-26 10:57:28 -0400

    6 14 navegar rutas nombres, tiene errores, func

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/alert_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -9,7 +11,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes APP',
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      //home: HomePage()
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/'       : (BuildContext context) => HomePage(),
+        'alert'   : (BuildContext context) => AlertPage(),
+        'avatar'  : (BuildContext context) => AvatarPage(),
+      },
     );
   }
 }
