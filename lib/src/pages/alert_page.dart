4b85ac33af38b00caa050e50583b8c4765b865b6
Alex <keremy.alex@gmail.com>
2020-05-26 15:43:49 -0400

    6 20 widget alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,12 +8,55 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: FlatButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: ()=> _mostrarAlert(context),
+
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add_location),
           onPressed: (){
             Navigator.pop(context);
           }
         ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) => AlertDialog(
+
+        //shape para redondear el contenedor de la alerta
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        
+        title: Text('Titulo'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('Este es el contenido de la caja de alerta'),
+            FlutterLogo(size: 100.0,)
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Cancelar'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text('Ok'),
+            onPressed: (){
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
