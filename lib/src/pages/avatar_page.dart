fc37bcbb4aa6a8599208eb762236f0b39655a6c6
Alex <keremy.alex@gmail.com>
2020-05-26 16:07:57 -0400

    6 21 widget circle avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRetACDq0iPMV99hEnXPOau74R1gbw-5ZOBZet4rIhU4jwJk_6-&s'),
+              radius: 25.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://cnnespanol2.files.wordpress.com/2018/11/181112135255-04-stan-lee-lead-image-full-169.jpg?quality=100&strip=info'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
