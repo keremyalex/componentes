db78fc84adddafa990ef761fec0814af2ffa86f9
Alex <keremy.alex@gmail.com>
2020-05-31 17:53:17 -0400

    6 30 ListView Builder c/Err

---
+++ lib/src/pages/listview_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ListaPage extends StatefulWidget {
+  @override
+  _ListaPageState createState() => _ListaPageState();
+}
+
+class _ListaPageState extends State<ListaPage> {
+  
+  List<int> _listaNumeros = [1,2,3,4,5];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listas'),
+      ),
+      body: _crearLista(),
+    );
+  }
+
+  Widget _crearLista(){
+    
+    return ListView.builder(
+      itemCount: _listaNumeros.length,
+      itemBuilder: (context, index) {
+        final imagen = _listaNumeros[index];
+
+        return FadeInImage(
+          image: NetworkImage('https://i.picsum.photos/id/$imagen/500/300.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        );
+      },
+    );
+  }
+
+}
