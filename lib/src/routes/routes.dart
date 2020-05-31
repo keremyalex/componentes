07b169393d02780046e54365c0ab170a4f305420
Alex <keremy.alex@gmail.com>
2020-05-31 12:03:24 -0400

    6 28 Slider

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
 
@@ -15,5 +16,6 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
         'card'    : (BuildContext context) => CardPage(),
         'animatedContainer'    : (BuildContext context) => AnimatedContainerPage(),
         'inputs'    : (BuildContext context) => InputPage(),
+        'slider'    : (BuildContext context) => SliderPage(),
     };
 }
